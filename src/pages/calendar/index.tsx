c0ec99074b60f0f5a1548b0b564a850707e15a9e
ranjeetkumar47 <rksingh6375@gmail.com>
2023-09-26 23:31:43 +0530

    hello

--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,7 +1,60 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Calendar = () => {
-  return <div>Calendar</div>
+  const url = 'https://dummyjson.com/products'
+  const [data, setData] = useState<any>()
+  const [cart, setCart] = useState<any>('')
+  const getData = async () => {
+    const response = await axios.get(url)
+    setData(response.data.products)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const handleAddtoCart = (product: any) => {
+    const productIndex = cart.findIndex((item: any) => item.id === product.id)
+    if (productIndex === -1) {
+      setCart([...cart, { ...product, quantity: 1 }])
+    } else {
+      const updatedCart = [...cart]
+      updatedCart[productIndex].quantity += 1
+    }
+  }
+
+  return (
+    <div>
+      <div>product lisiti</div>
+      {data &&
+        data.length !== 0 &&
+        data !== undefined &&
+        data?.map((item: any, index: any) => {
+          return (
+            <div className="flex">
+              <p>{item.title}</p>
+              <button onClick={() => handleAddtoCart(item)}>Add to Cart</button>
+            </div>
+          )
+        })}
+
+      <div>
+        <h3>Cart</h3>
+        {cart &&
+          cart.length !== 0 &&
+          cart !== undefined &&
+          cart?.map((item: any, index: any) => {
+            return (
+              <div className="flex">
+                <p>{item.title}</p>
+                <button>{item?.quantity}</button>
+              </div>
+            )
+          })}
+      </div>
+    </div>
+  )
 }
 
 export default Calendar
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { Menus } from '../../constants/menuList'
 
 const Dashboard = () => {
-  return <div>Dashboard</div>
+  return (
+    <div>
+      <div className="mt-8">
+        <h2 className="text-xl font-semibold mb-4">React Component</h2>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {Menus.map((item) => (
+            <div className="bg-white p-4 rounded-lg shadow-md hover:shadow-lg hover:scale-105 hover:border border-gray-200 transition duration-300">
+              <h3>{item.title}</h3>
+              <p>{item.title}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Dashboard
